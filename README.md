d11ca58c3c0f69acd0da53c983b22d9ac477bbb2
freewind <nowindlee@gmail.com>
2018-07-16 17:38:22 +0800

    table with array data demo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-JavaFX Table Demo
-=================
+JavaFX Table with Data Array Demo
+=================================
 
-Show a table with data.
+Show a table with data array, like:
+
+```
+new Object[]{"AAA", 111},
+new Object[]{"BBB", 222},
+new Object[]{"CCC", 333}
+```
 
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,30 +1,20 @@
 package demo;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.beans.property.SimpleStringProperty;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import static javafx.collections.FXCollections.observableArrayList;
-
 
 public class Hello extends Application {
 
@@ -37,38 +27,30 @@ public class Hello extends Application {
         primaryStage.setTitle("Hello");
         VBox root = new VBox() {{
             getChildren().add(createTable());
-            getChildren().add(createAddButton());
         }};
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
 
-    private final ObservableList<Person> data = observableArrayList(
-            new Person("AAA", 23, observableArrayList("Apple", "Banana")),
-            new Person("BBB", 11, observableArrayList("Pear")),
-            new Person("DDD", 34, observableArrayList("Orange"))
+    private final ObservableList<Object[]> data = observableArrayList(
+            new Object[]{"AAA", 111},
+            new Object[]{"BBB", 222},
+            new Object[]{"CCC", 333}
     );
 
-    private Button createAddButton() {
-        return new Button("Add Data") {{
-            setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                    data.add(new Person("NEW", new Random().nextInt(100), Arrays.asList("date", "watermelon")));
-                }
-            });
-        }};
-    }
-
-    private TableView<Person> createTable() {
-        return new TableView<Person>() {{
-            getColumns().add(new TableColumn<Person, String>("Name") {{
-                setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
+    private TableView<Object[]> createTable() {
+        return new TableView<Object[]>() {{
+            getColumns().add(new TableColumn<Object[], String>("String") {{
+                setCellValueFactory(param -> {
+                    String value = (String) param.getValue()[0];
+                    return new SimpleStringProperty(value);  // SimpleStringProperty is `ObservableValue`
+                });
             }});
-            getColumns().add(new TableColumn<Person, Integer>("Number") {{
-                setCellValueFactory(new PropertyValueFactory<Person, Integer>("number"));
-            }});
-            getColumns().add(new TableColumn<Person, List<String>>("Fruits") {{
-                setCellValueFactory(new PropertyValueFactory<Person, List<String>>("fruits"));
+            getColumns().add(new TableColumn<Object[], Integer>("Number") {{
+                setCellValueFactory(param -> {
+                    Integer value = (Integer) param.getValue()[1];
+                    return new SimpleIntegerProperty(value).asObject(); // SimpleIntegerProperty is not `ObservableValue`
+                });
             }});
             setItems(data);
             setEditable(true);
